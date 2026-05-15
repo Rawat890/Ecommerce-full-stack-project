0b3c3303320679ee95278a1421ff69b95bbf56d5
Rawat890 <rohanrawat7508@gmail.com>
2026-05-15 16:36:22 +0530

    feat: generateTokens and loginSignUp functions created

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const generateTokens = (user) => {
+ const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET,
+  { expiresIn: "2d" }
+ );
+
+ const refreshToken = jwt.sign({ userId: user._id }, process.env.REFRESH_TOKEN_SECRET,
+  { expiresIn: "5d" }
+ );
+
+ return { accessToken, refreshToken };
+}
+
+export const loginOrSignUp = async (req, res) => {
+ const { phone, address } = req.body;
+ try {
+  let user = await User.findOne({ phone });
+  if (!user) {
+   user = new User({ address, phone });
+   await user.save();
+  } else {
+   user.address = address;
+   await user.save();
+  }
+
+  const { accessToken, refreshToken } = generateTokens(user.toObject());
+
+  res.status(200).json({
+   user,
+   accessToken,
+   refreshToken
+  })
+ } catch (error) {
+  console.log("Login/signup error - ", error);
+  res.status(500).json({
+   error: error.message
+  })
+ }
+}
+

ab5c913513a1e16e2cb5eeb7523f8e546199491e
Rawat890 <rohanrawat7508@gmail.com>
2026-05-16 09:31:49 +0530

    feat: add order routes for creating orders and transactions

---
+++ Backend/src/routes/order.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { createOrder, createTransaction, getOrdersByUserId } from '../controllers/order.controller';
+
+const router = express.Router();
+
+router.post("/transaction", createTransaction);
+router.post("/:userId", getOrdersByUserId);
+router.post("/", createOrder);
+
+export default router;

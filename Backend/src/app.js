97cbc39fb3143037054998108dd58090969a6eaf
Rawat890 <rohanrawat7508@gmail.com>
2026-05-16 09:32:48 +0530

    feat: add routes for category, product, and order management

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import userRoutes from './routes/user.js';
+import categoryRoutes from './routes/category.js';
+import productRoutes from './routes/product.js';
+import orderRoutes from './routes/order.js';
 
 const app = express();
 app.use(express.json());
 
 //GET /user/get-data - importing the routes
 app.use('/user', userRoutes)
+app.use('/category', categoryRoutes);
+app.use('/product', productRoutes);
+app.use('/order', orderRoutes);
 
 export default app;

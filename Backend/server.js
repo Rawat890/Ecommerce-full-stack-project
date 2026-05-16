f863d59a38cf1a1f227f1905c7a1e8a938bae353
Rawat890 <rohanrawat7508@gmail.com>
2026-05-16 13:17:02 +0530

    feat: implement AdminJS user components and update server initialization

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,21 @@
-import app from "./src/app";
-import connectDB from "./src/db/db";
+import app from "./src/app.js";
+import { PORT } from "./src/config/config.js";
+import { buildAdminJS } from "./src/config/setup.js";
+import connectDB from "./src/db/db.js";
 
-const PORT = 3100;
+const startServer = async () => {
+  try {
+    await connectDB();
 
-connectDB();
+    await buildAdminJS(app);
 
-app.listen(PORT, () => {
- console.log(`Server started at port ${PORT}`)
-})
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}/admin`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();

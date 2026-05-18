28021b73714211deef41e05f67227339b57ffe70
Rawat890 <rohanrawat7508@gmail.com>
2026-05-18 12:20:10 +0530

    chore: useFonts hook used to load fonts

--- Mobile-frontend/src/App.tsx
+++ Mobile-frontend/src/App.tsx
@@ -1,7 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { fonts } from './utils/fonts';
+import { useFonts } from 'expo-font';
 
 const App = () => {
+  const [loaded] = useFonts({
+    [fonts.notoBold]: require('./assets/fonts/NotoSans-Bold.ttf'),
+    [fonts.notoMedium]: require('./assets/fonts/NotoSans-Medium.ttf'),
+    [fonts.notoRegular]: require('./assets/fonts/NotoSans-Regular.ttf'),
+    [fonts.notoSemi]: require('./assets/fonts/NotoSans-SemiBold.ttf'),
+  })
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <View>
       <Text>App</Text>

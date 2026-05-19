1f6bf9ea47e1f9d88dc7831e08b03cafe6fbab89
Rawat890 <rohanrawat7508@gmail.com>
2026-05-19 21:19:03 +0530

    chore: categories slice created and reducers defined

---
+++ Mobile-frontend/src/modules/categories/api/slice.tsx
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const categoriesSlice = createSlice({
+ name: 'categories',
+ initialState: {
+  data: null,
+  loading: false,
+  error: null
+ },
+ reducers: {
+  setLoading: (state) => {
+   state.loading = true
+  },
+  setData: (state, action) => {
+   state.loading = false;
+   state.data = action.payload;
+  },
+  setError: (state, action) => {
+   state.loading = false;
+   state.error = action.payload;
+  }
+ }
+})
+
+export const { setLoading, setData, setError } = categoriesSlice.actions;
+export default categoriesSlice.reducer;

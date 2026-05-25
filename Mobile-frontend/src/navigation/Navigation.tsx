ac944ca69828ef5b3173555d6a4568d60c4d9c12
Rawat890 <rohanrawat7508@gmail.com>
2026-05-25 17:47:19 +0530

    chore: Products screen added in stack

--- Mobile-frontend/src/navigation/Navigation.tsx
+++ Mobile-frontend/src/navigation/Navigation.tsx
@@ -1,4 +1,3 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -6,6 +5,7 @@ import { SCREENS } from '../utils/routes';
 import { navigationRef } from '../utils/navigationService';
 import Splash from '../modules/onboard';
 import MainNavigator from './MainNavigator';
+import Products from '../modules/products';
 
 const Stack = createNativeStackNavigator();
 const Navigation = () => {
@@ -19,6 +19,7 @@ const Navigation = () => {
    >
     <Stack.Screen name={SCREENS.Splash} component={Splash} />
     <Stack.Screen name={SCREENS.MainNavigator} component={MainNavigator} />
+    <Stack.Screen name={SCREENS.Products} component={Products} />
    </Stack.Navigator>
   </NavigationContainer>
  )

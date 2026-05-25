17d73c6ce8247b26f5304c2829b576c74806634b
Rawat890 <rohanrawat7508@gmail.com>
2026-05-25 17:45:49 +0530

    chore: categoriesSaga added in rootSaga

--- Mobile-frontend/src/store/rootSaga.tsx
+++ Mobile-frontend/src/store/rootSaga.tsx
@@ -1,8 +1,10 @@
+import categoriesSaga from "../modules/categories/api/saga"
 import homeSaga from "../modules/home/api/saga"
 import {all, fork} from 'redux-saga/effects'
 
 export function* rootSaga() {
  yield all([
-  fork(homeSaga)
+  fork(homeSaga),
+  fork(categoriesSaga)
  ])
 }
